7810bec9a16bb1d22ee63fe61f1c0f4f094eb299
Parul Pandey <parulss1606@gmail.com>
2025-08-03 23:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+  s = s.trim();
+
+  // Find the index of first space
+  const index = s.indexOf(" ");
+
+  // If there's no space, return the whole string
+  if (index === -1) return s;
+
+  // Return substring up to the first space
+  return s.substring(0, index);
 }
 
 // Do not change the code below
